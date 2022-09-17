07de6a6de8ad86211a47b9b1db9deae8324c1714
wmz786 <waismalikzada9@gmail.com>
2022-09-17 15:49:03 +0430

    alternative to module

---
+++ alternative.js
@@ -0,0 +1,7 @@
+module.exports.item = ["item1", "item2"];
+
+const car = {
+  name: "Volvo",
+};
+
+module.exports.singleCar = car;
--- app.js
+++ app.js
@@ -6,6 +6,8 @@
 
 const names = require("./names");
 const sayHi = require("./utils");
+const alt = require("./alternative");
 
+console.log(alt);
 sayHi(names.ahmad);
 sayHi(names.john);
