52b12cbb515d84c529d4c1aa60f8e79d3ee74dd0
wmz786 <waismalikzada9@gmail.com>
2022-09-18 16:34:58 +0430

    difference between sync and async fs module

--- fs-sync.modules.js
+++ fs-sync.modules.js
@@ -1,9 +1,13 @@
 const { readFileSync, writeFileSync } = require("node:fs");
 
+console.log("start");
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
 console.log(first, second);
+console.log("done with this task");
 
 writeFileSync("./content/result-sync.txt", `Result : ${first} , ${second}`, {
   flag: "a",
 });
+
+console.log("start a new task");
