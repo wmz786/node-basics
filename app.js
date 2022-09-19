c0261a823b96aa2df47046930ee135db8633e409
wmz786 <waismalikzada9@gmail.com>
2022-09-19 12:18:38 +0430

    streams with http example

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-const { createReadStream } = require("node:fs");
 
-const stream = createReadStream("./content/big.txt", {
-  highWaterMark: 90000,
-  encoding: "utf8",
-});
-
-stream.on("data", (result) => {
-  console.log(result);
-});
---
+++ streams.js
@@ -0,0 +1,10 @@
+const { createReadStream } = require("node:fs");
+
+const stream = createReadStream("./content/big.txt", {
+  highWaterMark: 90000,
+  encoding: "utf8",
+});
+
+stream.on("data", (result) => {
+  console.log(result);
+});
